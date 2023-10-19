f5fc60d3c255a924724484dfa89573d54dce593d
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2023-10-19 18:49:41 +0530

    Update general.js get by filter

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,21 +12,42 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  res.send(JSON.stringify(books))});
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+      return res.json(book);
+    } else {
+      return res.status(404).send('Book not found');
+    }
+  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const books_current_author = [];
+    const isbnList = Object.keys(books);
+    console.log(isbnList)
+    for (const id of isbnList) {
+      const book = books[id];
+      console.log(book.author)
+      if (book.author === author) {
+        books_current_author.push(book);
+      }
+    }
+  
+    const numberOfBooks = books_current_author.length;
+    console.log(`Number of books by ${author}: ${numberOfBooks}`);
+    console.log('List of books:', books_current_author);
+  
+    // You can send the books_current_author array as a JSON response
+    res.send(books_current_author);
+  });
+  
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
